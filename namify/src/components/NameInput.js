6e8e48c128536d8c6b2c19240632c93de7f27acf
Hrithik Shah <hrithikchamp@gmail.com>
2020-08-12 18:02:28 -0400

    submit button does something

--- namify/src/components/NameInput.js
+++ namify/src/components/NameInput.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import './NameInput.css';
 
-const NameInput = () => {
-    return (
-        <div className="container">
-            <div className="inner-container">
-                <input type='text' id='name-input'></input>
+class NameInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit(event) {
+        document.getElementById('thanks').innerHTML = 'A name was submitted: ' + this.state.value;
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="inner-container">
+                    <form onSubmit={this.handleSubmit}>
+                        <input type='text' id='name-input' value={this.state.value} onChange={this.handleChange}></input>
+                        <input type="submit" value="Submit" on />
+                    </form>
+                    <h2 id='thanks'> </h2>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 
