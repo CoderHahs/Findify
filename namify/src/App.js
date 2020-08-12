6a208def818a4a0944031e38e466ca35157c974c
Hrithik Shah <hrithikchamp@gmail.com>
2020-08-11 20:00:51 -0400

    input + styling

--- namify/src/App.js
+++ namify/src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import NameInput from './components/NameInput'
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="namify-header">
-        <h1 className="logo">Namify</h1>
-      </header> */}
       <header id="sd-topbar" class="sd-container style-module--top-bar--8EncK  style-module--revealed--23Rha">
         <div class="sd-container-inner">
           <div class="style-module--inner--1qRHH">
             <a aria-label="Go to homepage" class="style-module--logo--10oUe" href="/"><h1>Namify</h1></a>
             <nav aria-label="site navigation" class="style-module--desktop-nav--22t5t">
               <ul class="unstyled-list style-module--navigation--Xeg87">
-                <li><a class="t-ui-2 style-module--item--2doqO " href="/stories"><span>About</span></a></li>
-                <li><a class="t-ui-2 style-module--item--2doqO " href="/stories/listen"><span>Hire me!</span></a></li>
+                <li><a class="t-ui-2 style-module--item--2doqO " href="/about"><span>About</span></a></li>
+                <li><a class="t-ui-2 style-module--item--2doqO " href="/hire-me"><span>Hire me!</span></a></li>
               </ul>
             </nav>
           </div>
         </div>
       </header>
+
+      <body>
+        <NameInput />
+      </body>
     </div>
   );
 }
---
+++ namify/src/components/NameInput.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './NameInput.css';
+
+const NameInput = () => {
+    return (
+        <div className="container">
+            <div className="inner-container">
+                <input type='text' id='name-input'></input>
+            </div>
+        </div>
+    );
+}
+
+
+export default NameInput;
